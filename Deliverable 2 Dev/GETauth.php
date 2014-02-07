26e71199b7516eb5d7cd37092edde7b2928c3072
Daniel Cohen <analoguecolour@gmail.com>
2014-02-07 17:07:03 +0000

    fixed db table
    stupid uppercase.

--- Deliverable 2 Dev/GETauth.php
+++ Deliverable 2 Dev/GETauth.php
@@ -1,6 +1,6 @@
 <?php
 
-    $sql="SELECT Username, Password FROM userTable";
+    $sql="SELECT Username, Password FROM UserTable";
     include "DBquery.php";
     $JSON = json_encode($res->fetchAll());
     echo $JSON;
