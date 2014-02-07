1d9b6ced720bb5e8160ecdf5f4b89f80c1ed4600
nikkwilliams <n.t.williams-12@student.lboro.ac.uk>
2014-02-07 22:53:51 +0000

    Fixed get requests sql

--- Deliverable 2 Dev/GETallRequests.php
+++ Deliverable 2 Dev/GETallRequests.php
@@ -1,7 +1,6 @@
 <?php
 
-    $id = $_GET["id"];
-    $sql="SELECT * FROM EntryRequestTable, ModuleTable";
+    $sql="SELECT * FROM EntryRequestTable INNER JOIN ModuleTable ON EntryRequestTable.ModuleCode=ModuleTable.ModuleCode";
     include "DBquery.php";
     $JSON = json_encode($res->fetchAll());
     echo $JSON;
