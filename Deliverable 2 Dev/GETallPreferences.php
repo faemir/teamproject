676f1cf1c26b6802461589b67bf60bc6f5643316
nikkwilliams <n.t.williams-12@student.lboro.ac.uk>
2014-02-11 10:32:16 +0000

    SQL to get preferences from database

---
+++ Deliverable 2 Dev/GETallPreferences.php
@@ -0,0 +1,8 @@
+<?php
+	$username = "admin";
+    $sql="SELECT * FROM Preferences WHERE username='$username'";
+    include "DBquery.php";
+    $JSON = json_encode($res->fetchAll());
+    echo $JSON;
+
+?>
