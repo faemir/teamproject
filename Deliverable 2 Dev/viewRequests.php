ae4f5b787334fcdf2bcd460906303aa027dd0200
nikkwilliams <n.t.williams-12@student.lboro.ac.uk>
2014-02-11 10:31:59 +0000

    Filtering tables headers complete

--- Deliverable 2 Dev/viewRequests.php
+++ Deliverable 2 Dev/viewRequests.php
@@ -13,9 +13,46 @@
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script type="text/javascript">
-        //onload
+        //onload ----------------------------------------------------------------//
+		$(document).ready(function(){getUserPrefs();});
         $(document).ready(function(){wrRequestsTable();});
+		// GLOBALS -----------------------------------------------------------------//
+		var viewHeaders = new Array();
+		var headersArray = new Array("Module Code", "Module Title", "Priority", "Year", "Semester", "Day", "Start Time", "End Time", "Period", "Duration", "No Of Students", "No Of Rooms", "Preferred Rooms", "Quality Room", "Wheelchair Access", "Data Projector", "Double Projector", "Visualiser", "Video/DVD/BluRay", "Computer", "White Board", "Chalk Board");
+		var way = "1";
+		var userPrefPeriod = "";
+		var userPrefTimeformat = "";
+		var userPrefStartWeek = "";
+		var userPrefEndWeek = "";
+		var userPrefLocation = "";
+		var userPrefHeader1 = "";
+		var userPrefHeader2 = "";
+		var userPrefHeader3 = "";
+		var userPrefHeader4 = "";
+		var userPrefHeader5 = "";
+		var userPrefHeader6 = "";
+		
+		// MAIN FUNCTIONS ---------------------------------------------------------------------------------------//
+		
+		function getUserPrefs(){
+			$.ajax({
+				type: "GET",
+				dataType: "json",
+				url: "GETallPreferences.php",
+				//data: username from dan,
+				success: function(JSON){
+					userPrefHeader1 = JSON[0].header1;
+					userPrefHeader2 = JSON[0].header2;
+					userPrefHeader3 = JSON[0].header3;
+					userPrefHeader4 = JSON[0].header4;
+					userPrefHeader5 = JSON[0].header5;
+					userPrefHeader6 = JSON[0].header6;
+				}
+			});
 
+		}
+		
+		//Rewrite with for loops from a GET from preferences table Header 1-6 changing number to writing..
         function wrRequestsTable(){
             //writes and populates Requests table. needs preferences input
             $.ajax({
@@ -23,30 +60,91 @@
                 dataType: "json",
                 url: "GETallRequests.php",
                 success: function(JSON){
-
                     var codeStr = "";
                     codeStr += '<table id="RequestsTable">';
                     codeStr += '<tr>';
                     //Added sorting of th's
-                    codeStr += '    <thonclick="sortTable(0)">Module Code</th>';
-                    codeStr += '    <thonclick="sortTable(1)">Priority</th>';
-                    codeStr += '    <thonclick="sortTable(2)">Day</th>';
-                    codeStr += '    <thonclick="sortTable(3)">Period</th>';
-                    codeStr += '    <thonclick="sortTable(4)">Details</th>';
-                    codeStr += '    <thonclick="sortTable(5)">Status</th>';
+					viewHeaders[0] = userPrefHeader1;
+					viewHeaders[1] = userPrefHeader2;
+					viewHeaders[2] = userPrefHeader3;
+					viewHeaders[3] = userPrefHeader4;
+					viewHeaders[4] = userPrefHeader5;
+					viewHeaders[5] = userPrefHeader6;
+					var countersort = 0;
+					for (var z=0;z < viewHeaders.length;z++){
+						codeStr += '	<th onclick="sortTable(' + countersort + ')">' + headersArray[viewHeaders[z]] + '</th>';
+						countersort += 1;
+					}
+                    codeStr += '    <th onclick="sortTable(6)">Details</th>';
+                    codeStr += '    <th onclick="sortTable(7)">Status</th>';
                     codeStr += '</tr>';
 
                     for(var i=0;i<JSON.length;i++){
                         codeStr += '<tr>';
-                        codeStr += '    	<td>' + JSON[i].modulecode + '</td>';
-                        codeStr += '    	<td>' + JSON[i].priority + '</td>';
-                        codeStr += '    	<td>' + JSON[i].day + '</td>';
-                        codeStr += '    	<td>' + JSON[i].period + '</td>';
+						for (var h=0;h<6;h++){
+							var starttime = JSON[i].period + 8;
+							if (starttime == 9)
+								starttime = "0" + starttime;
+							starttime = starttime + ":00";
+							var endtime = parseInt(JSON[i].period) + parseInt(JSON[i].duration) + 7;
+							if (endtime == 9)
+								endtime = "0" + endtime;
+							endtime = endtime + ":50";
+							
+							if(viewHeaders[h] == "0")
+								codeStr += '    	<td>' + JSON[i].modulecode + '</td>';
+							else if(viewHeaders[h] == "1")
+								codeStr += '    	<td>' + JSON[i].moduletitle + '</td>';
+							else if(viewHeaders[h] == "2")
+								codeStr += '    	<td>' + JSON[i].priority + '</td>';
+							else if(viewHeaders[h] == "3")
+								codeStr += '    	<td>' + JSON[i].year + '</td>';
+							else if(viewHeaders[h] == "4")
+								codeStr += '    	<td>' + JSON[i].semester + '</td>';
+							else if(viewHeaders[h] == "5")
+								codeStr += '    	<td>' + JSON[i].day + '</td>';
+							else if(viewHeaders[h] == "6")
+								codeStr += '    	<td>' + starttime + '</td>';
+							else if(viewHeaders[h] == "7")
+								codeStr += '    	<td>' + endtime + '</td>';
+							else if(viewHeaders[h] == "8")
+								codeStr += '    	<td>' + JSON[i].period + '</td>';
+							else if(viewHeaders[h] == "9")
+								codeStr += '    	<td>' + JSON[i].duration + '</td>';
+							else if(viewHeaders[h] == "10")
+								codeStr += '    	<td>' + JSON[i].noofstudents + '</td>';
+							else if(viewHeaders[h] == "11")
+								codeStr += '    	<td>' + JSON[i].noofrooms + '</td>';
+							else if(viewHeaders[h] == "12")
+								codeStr += '    	<td>' + JSON[i].preferredrooms + '</td>';
+							else if(viewHeaders[h] == "13")
+								codeStr += '    	<td>' + JSON[i].qualityroom + '</td>';
+							else if(viewHeaders[h] == "14")
+								codeStr += '    	<td>' + JSON[i].wheelchairaccess + '</td>';
+							else if(viewHeaders[h] == "15")
+								codeStr += '    	<td>' + JSON[i].dataprojector + '</td>';
+							else if(viewHeaders[h] == "16")
+								codeStr += '    	<td>' + JSON[i].doubleprojector + '</td>';
+							else if(viewHeaders[h] == "17")
+								codeStr += '    	<td>' + JSON[i].visualiser + '</td>';
+							else if(viewHeaders[h] == "18")
+								codeStr += '    	<td>' + JSON[i].videodvdbluray + '</td>';
+							else if(viewHeaders[h] == "19")
+								codeStr += '    	<td>' + JSON[i].computer + '</td>';
+							else if(viewHeaders[h] == "20")
+								codeStr += '    	<td>' + JSON[i].whiteboard + '</td>';
+							else if(viewHeaders[h] == "21")
+								codeStr += '    	<td>' + JSON[i].chalkboard + '</td>';
+						}
+							
+						
+						
                         codeStr += '    	<td><input type="button" class="detailsButton" value="Details" onclick="showDetails(' + JSON[i].requestid + ')"></input></td>';
                         codeStr += '    	<td>' + JSON[i].requeststatus + '</td>';
                         codeStr += '	</tr>';
                     }
-                    codeStr += "</table">
+                    codeStr += "</table>";
+					alert(codeStr);
 
                     //clears and writes table into container
                     // UPDATE: empty function produced 'false' onscreen when already empty
@@ -66,17 +164,17 @@
         }
 
 		//Sort functions. Asc, Desc alternating. Bubble sort.
-		var way = "1";
+		
 		function sortTable(colnumber){
 			//Fill 2D array with each row of table.
 			var value=new Array();
 			var rows = RequestsTable.getElementsByTagName('tr');
-			for (i = 1; i < rows.length; i++){
+			for (var i = 1; i < rows.length; i++){
 				var row = rows[i];
 				var cols = row.getElementsByTagName('td');
 				var row_data = [];
 				value[i]=new Array();
-				for (j = 0; j < cols.length; j++){
+				for (var j = 0; j < cols.length; j++){
 					value[i][j]=cols[j].innerHTML;
 				}
 			}
@@ -86,7 +184,7 @@
 				var swapped = true;
 				while(swapped){
 					swapped = false;
-					for (k = 1; k < value.length-1; k++){
+					for (var k = 1; k < value.length-1; k++){
 						if (value[k][colnumber] > value[k+1][colnumber]){
 							temp[0]=value[k];
 							value[k]=value[k+1];
@@ -102,7 +200,7 @@
 				var swapped2 = true;
 				while(swapped2){
 					swapped2 = false;
-					for (m = 1; m < value.length-1; m++){
+					for (var m = 1; m < value.length-1; m++){
 						if (value[m][colnumber] < value[m+1][colnumber]){
 							temp[0]=value[m];
 							value[m]=value[m+1];
@@ -113,16 +211,24 @@
 				}
 				way = "1";
 			}
-			//Recreate table after sorting
+			// Recreate table after sorting
 			var codeStr = "";
 			codeStr += '<table id="RequestsTable">';
 			codeStr += '<tr>';
-			codeStr += '    <th onclick="sortTable(0)">Module Code</th>';
-			codeStr += '    <th onclick="sortTable(1)">Priority</th>';
-			codeStr += '    <th onclick="sortTable(2)">Day</th>';
-			codeStr += '    <th onclick="sortTable(3)">Period</th>';
-			codeStr += '    <th onclick="sortTable(4)">Details</th>';
-			codeStr += '    <th onclick="sortTable(5)">Status</th>';
+			var countersort = 0;
+			for (var z=0;z<viewHeaders.length;z++){
+				codeStr += '	<th onclick="sortTable(' + countersort + ')">' + headersArray[viewHeaders[z]] + '</th>';
+				countersort += 1;
+			}
+			// codeStr += '    <th onclick="sortTable(0)">Module Code</th>';
+			// codeStr += '    <th onclick="sortTable(1)">Priority</th>';
+			// codeStr += '    <th onclick="sortTable(2)">Day</th>';
+			// codeStr += '    <th onclick="sortTable(3)">Period</th>';
+			// codeStr += '    <th onclick="sortTable(4)">blahh</th>';
+			// codeStr += '    <th onclick="sortTable(4)">blahh2</th>';
+
+			codeStr += '    <th onclick="sortTable(6)">Details</th>';
+			codeStr += '    <th onclick="sortTable(7)">Status</th>';
 			codeStr += '</tr>';
 			for(var l=1;l<value.length;l++){
 				codeStr += '	<tr>';
@@ -132,6 +238,8 @@
 				codeStr += '    	<td>' + value[l][3] + '</td>';
 				codeStr += '    	<td>' + value[l][4] + '</td>';
 				codeStr += '    	<td>' + value[l][5] + '</td>';
+				codeStr += '    	<td>' + value[l][6] + '</td>';
+				codeStr += '    	<td>' + value[l][7] + '</td>';
 				codeStr += '	</tr>';
 			}
 			codeStr += '</table>';
@@ -139,7 +247,6 @@
 			document.getElementById("tableBox").innerHTML = "";
 			document.getElementById("tableBox").innerHTML = codeStr;
 		}
-
         </script>
     </head>
 
@@ -154,9 +261,9 @@
             </ul>
         </div>
         <div id="pagewrap">
-            <div class="contentBox" id="actionsBox"></div>
-
             <div class="contentBox" id="searchBox"></div>
+
+            <div class="contentBox" id="filterBox"></div>
 
             <div class="contentBox" id="roundsBox"></div>
 
