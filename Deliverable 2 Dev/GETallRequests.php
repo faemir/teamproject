a850088e619fd171dfb35ec9edddde2edb2f2b44
Clowny <m.cole-12@student.lboro.ac.uk>
2014-02-12 23:40:14 +0000

    get all requests completed

--- Deliverable 2 Dev/GETallRequests.php
+++ Deliverable 2 Dev/GETallRequests.php
@@ -4,19 +4,22 @@
 	$type = $_GET['type'];
 	$searchval = $_GET['searchval'];
 	$typearray = array("EntryRequestTable.modulecode","moduletitle","day","requeststatus","period", "duration", "priority", "noofstudents","qualityroom","preferredrooms","semester", "noofrooms", "wheelchairaccess", "dataprojector", "doubleprojector", "visualiser", "videodvdbluray", "computer", "whiteboard", "chalkboard");
+	$username = 'admin';
+	//$username = $_session['username'];
 	
-	
-	$sql="SELECT * FROM EntryRequestTable INNER JOIN ModuleTable ON EntryRequestTable.ModuleCode=ModuleTable.ModuleCode ";
-	if($semsval != '0' AND $type == ""){
-		$sql .= "WHERE semester = '$semsval';";
+	$sql="SELECT * FROM EntryRequestTable INNER JOIN ModuleTable ON EntryRequestTable.ModuleCode=ModuleTable.ModuleCode
+	INNER JOIN DepartmentTable ON ModuleTable.departmentid=DepartmentTable.departmentid
+	INNER JOIN UserTable ON DepartmentTable.departmentid=UserTable.departmentid
+	WHERE UserTable.username='$username' ";
+	if($semsval != '0'){
+		$sql .= "AND semester = '$semsval';";
 		
 	}
-	else if($type != "" AND $semsval == '0'){
-		$sql .= "WHERE $typearray[$type] LIKE '%$searchval%';";
+	if($type != ""){
+		$sql .= "AND $typearray[$type] LIKE '%$searchval%';";
 	}
-	else if($semsval != '0' AND $type != ""){
-		$sql .= "WHERE $typearray[$type] LIKE '%$searchval%' AND semester = '$semsval';";	
-	}
+	
+	
 
    include "DBquery.php";
    $JSON = json_encode($res->fetchAll());
