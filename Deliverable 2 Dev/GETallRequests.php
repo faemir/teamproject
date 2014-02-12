8992fce4cb05465084fe8a2230bc5b601e601999
nikkwilliams <n.t.williams-12@student.lboro.ac.uk>
2014-02-12 01:16:10 +0000

    Fixed sql to get all request information

--- Deliverable 2 Dev/GETallRequests.php
+++ Deliverable 2 Dev/GETallRequests.php
@@ -1,6 +1,12 @@
 <?php
-
-    $sql="SELECT * FROM EntryRequestTable INNER JOIN ModuleTable ON EntryRequestTable.ModuleCode=ModuleTable.ModuleCode";
+	
+	$username = 'admin';
+	//$username = $_session['username'];
+    $sql="SELECT * FROM EntryRequestTable 
+	INNER JOIN ModuleTable ON EntryRequestTable.modulecode=ModuleTable.modulecode
+	INNER JOIN DepartmentTable ON ModuleTable.departmentid=DepartmentTable.departmentid
+	INNER JOIN UserTable ON DepartmentTable.departmentid=UserTable.departmentid
+	WHERE UserTable.username='$username'";
     include "DBquery.php";
     $JSON = json_encode($res->fetchAll());
     echo $JSON;
