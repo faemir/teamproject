2b7767671e22c97fb8f41be26e8ba14082bf13f5
nikkwilliams <n.t.williams-12@student.lboro.ac.uk>
2014-02-13 13:17:10 +0000

    Fixed getting preferences

--- Deliverable 2 Dev/GETallPreferences.php
+++ Deliverable 2 Dev/GETallPreferences.php
@@ -1,6 +1,6 @@
 <?php
 
-	$username = "cojs12";
+	$username = "admin";
     $sql="SELECT * FROM Preferences WHERE username='$username'";
     include "DBquery.php";
     $JSON = json_encode($res->fetchAll());
