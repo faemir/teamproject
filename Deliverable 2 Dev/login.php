d07c5e9138c35f672b3f13eb1bea53b271ef2ea1
Daniel Cohen <analoguecolour@gmail.com>
2014-02-13 02:13:51 +0000

    moved auth from login.php to auth.php

--- Deliverable 2 Dev/login.php
+++ Deliverable 2 Dev/login.php
@@ -10,37 +10,6 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <link href='cssTimetable.css' rel='stylesheet' type='text/css'>
         <title>Team 4 - LU Timetable Login</title>
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script type="text/javascript">
-
-
-            function checkusername(){
-                var auth=false;
-				$.get("GETauth.php",function(JSON){
-                    var sessid = "<?php echo SID ?>";
-                    var users = "";
-                    var passes = "";
-                    var userUser = "<?php echo $_SESSION['username']; ?>";
-                    var userPass = "<?php echo $_SESSION['password']; ?>";
-                    for(var i=0;i<JSON.length;i++){
-                        console.log("in the loop");
-                        users = JSON[i].username;
-                        passes = JSON[i].password;
-                        console.log(userUser);
-                        console.log(userPass);
-                        console.log(users);
-                        console.log(passes);
-                        if (users == userUser && passes == userPass)
-                            auth=true;
-                        }
-                        if (auth == true){
-                            console.log("authorised");
-                            window.location.replace("viewRequests.php?" + sessid);
-                        }
-				},'JSON');
-
-            }
-        </script>
     </head>
 
     <body>
