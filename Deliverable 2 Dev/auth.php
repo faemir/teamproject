48f8ec3a5cb9ffdbd66bfcd0bf009418a90ea0ce
Daniel Cohen <analoguecolour@gmail.com>
2014-02-13 02:13:51 +0000

    moved auth from login.php to auth.php

--- Deliverable 2 Dev/auth.php
+++ Deliverable 2 Dev/auth.php
@@ -5,6 +5,36 @@
     session_start();
 ?>
 <html>
+<head>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript">
+function checkusername(){
+    var auth=false;
+		$.get("GETauth.php",function(JSON){
+    var sessid = "<?php echo SID ?>";
+    var users = "";
+    var passes = "";
+    var userUser = "<?php echo $_SESSION['username']; ?>";
+    var userPass = "<?php echo $_SESSION['password']; ?>";
+    for(var i=0;i<JSON.length;i++){
+        console.log("in the loop");
+        users = JSON[i].username;
+        passes = JSON[i].password;
+        console.log(userUser);
+        console.log(userPass);
+        console.log(users);
+        console.log(passes);
+        if (users == userUser && passes == userPass)
+            auth=true;
+        }
+        if (auth == true){
+            console.log("authorised");
+            window.location.replace("viewRequests.php?" + sessid);
+        }
+		},'JSON');
+}
+</script>
+</head>
 <body>
 <?php
     $_SESSION['username']=$_POST['user_input'];
