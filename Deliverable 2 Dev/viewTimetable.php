ab03c2c86277063a8dde0ba2b32313554544b631
Clowny <clownyboyxd@gmail.com>
2014-02-15 23:17:29 +0000

    more updating....

--- Deliverable 2 Dev/viewTimetable.php
+++ Deliverable 2 Dev/viewTimetable.php
@@ -99,99 +99,161 @@
 			function clickedtable(value){
 				if($(value).attr('class')=='changeVT'){
 					if(reqID.length != 0){
-						var seleID =0;
+						var seleID =[];
+						var RoomArr=[];
+						var RoomIdArr = [];
 						for(var count = 0; count < reqID.length; count++){
 							if (reqID[count][0] == value.id){
-								seleID = reqID[count][1];
+								seleID[seleID.length] = reqID[count][1];
 							}
 						}
-						if (seleID!=0){
+						codeStl = "";
+						for(var i = 0; i < seleID.length; i++){
 							$.ajax({
 								type: "GET",
 								dataType: "json",
 								url: "GETdetailedRequestsTimetable.php",
 								async: false,
-								data: {'id':seleID},
+								data: {'id':seleID[i]},
 								success: function(JSON){
-									var codeStl = "<table id='detailsTable'>";
-				
-									codeStl += "<tr>";
-									codeStl += "<td>Request ID: " + JSON[0].requestid + "</td>";
-									codeStl += "<td>Year: " + JSON[0].year + "</td>";
-									codeStl += "</tr>";
-									codeStl += "<tr>";
-									codeStl += "<td>Module Code: " + JSON[0].modulecode + "</td>";
-									codeStl += "</tr>";
-									codeStl += "<tr>";
-									codeStl += "<td colspan='2'>" + JSON[0].moduletitle + "</td>";
-									codeStl += "</tr>";
-									codeStl += "<tr>";
-									codeStl += "<td>Period: " + JSON[0].period + "</td>";
-									codeStl += "<td>Duration: " + JSON[0].duration + "</td>";
-									codeStl += "</tr>";
-									codeStl += "<tr>";
-									
-									/* if (timeFormat==1){
-										var Starttime = startTimeList1.concat(startTimeList2);
-										var Endtime = endTimeList1.concat(endTimeList2)
+									for(var j = 0; j < JSON.length; j++){
+										if(RoomIdArr[j]==0){
+											RoomIdArr[RoomIdArr.length]=JSON[j].roomid;
+										}else{
+											var boolcheck = true;
+											for (var i = 0; i < RoomIdArr.length; i++){
+												if(RoomIdArr[i]==JSON[j].roomid){
+													boolcheck = false;
+												}
+											}
+											if(boolcheck){
+												RoomIdArr[RoomIdArr.length] = JSON[j].roomid;
+											}
+										}
 									}
-									else{
-										var Starttime = startTimeList1.concat(startTimeList3);
-										var Endtime = endTimeList1.concat(endTimeList3);
+									for(var j = 0; j < JSON.length; j++){
+										var subRoomArr =[];
+										subRoomArr[0] = JSON[j].requestid;
+										subRoomArr[1] = JSON[j].year;
+										subRoomArr[2] = JSON[j].modulecode;
+										subRoomArr[3] = JSON[j].moduletitle;
+										subRoomArr[4] = JSON[j].period;
+										subRoomArr[5] = JSON[j].day;
+										subRoomArr[6] = JSON[j].noofstudents;
+										subRoomArr[7] = JSON[j].noofrooms;
+										subRoomArr[8] = JSON[j].qualityroom;
+										subRoomArr[9] = JSON[j].wheelchairaccess;
+										subRoomArr[10] = JSON[j].dataprojector;
+										subRoomArr[11] = JSON[j].doubleprojector;
+										subRoomArr[12] = JSON[j].visualiser;
+										subRoomArr[13] = JSON[j].videodvdbluray;
+										subRoomArr[14] = JSON[j].computer;
+										subRoomArr[15] = JSON[j].whiteboard;
+										subRoomArr[16] = JSON[j].chalkboard;
+										subRoomArr[17] = JSON[j].nearestroom;
+										if(RoomArr.length==0){
+											subRoomArr[18] = RoomIdArr;
+											RoomArr[RoomArr.length] = subRoomArr;
+										}else{
+											var boolcheck = true;
+											for (var l = 0; l < RoomArr.length; l++){
+												if(RoomArr[l][0]==subRoomArr[0] && RoomArr[l][4]==subRoomArr[4] && RoomArr[l][5]==subRoomArr[5]){
+													boolcheck = false;
+												}
+											}
+											if(boolcheck){
+												subRoomArr[18] = RoomIdArr;
+												RoomArr[RoomArr.length] = subRoomArr;
+											}
+										}
+										
 									}
-									var starttime = Starttime[JSON[0].period-1];
-									var endtime = Endtime[(parseInt(JSON[0].period) + parseInt(JSON[0].duration)-2)];
-									
-									codeStl += "<td colspan='2'>Requested Time: " + starttime + "-" + endtime + "</td>"; */
-									codeStl += "</tr>";
-									codeStl += "<tr>";
-									codeStl += "<td>No. Students: " + JSON[0].noofstudents + "</td>";
-									codeStl += "<td>No. Rooms: " + JSON[0].noofrooms + "</td>";
-									codeStl += "</tr>";
-									codeStl += "<tr>";
-									if(JSON[0].preferredrooms==1){
-										codeStl += "<td colspan='2'>Preferred room: "+ JSON[0].roomid +"</td>";
-									}
-									codeStl += "<tr><td colspan='2'>";
-									if(JSON[0].qualityroom==1){
-										codeStl += "Quality Room, ";
-									}
-									if(JSON[0].wheelchairaccess==1){
-										codeStl += "Wheelchair Access, ";
-									}
-									if(JSON[0].dataprojector==1){
-										codeStl += "Data Projector, ";
-									}
-									if(JSON[0].doubleprojector==1){
-										codeStl += "Double Projector, ";
-									}
-									if(JSON[0].visualiser==1){
-										codeStl += "Visualiser, ";
-									}
-									if(JSON[0].videodvdbluray==1){
-										codeStl += "Video/DVD/Bluray, ";
-									}
-									if(JSON[0].computer==1){
-										codeStl += "Computer, ";
-									}
-									if(JSON[0].whiteboard==1){
-										codeStl += "White Board, ";
-									}
-									if(JSON[0].chalkboard==1){
-										codeStl += "Chalk Board, ";
-									}
-									if(JSON[0].nearestroom==1){
-										codeStl += "Nearest Room, ";
-									}
-									codeStl = codeStl.substring(0,codeStl.length-2);
-									codeStl += ".</tr>";
-									
-									document.getElementById("roomSelectorBox2").innerHTML = (codeStl);
+									wrTables(RoomArr);
 								}
 							});
 						}
 					}
 				}
+			}
+			function wrTables(RoomArr){
+				var codeStl = "";
+				for(var i = 0; i < RoomArr.length; i++){
+					codeStl += "<table id='detailsTable'>";
+					codeStl += "<tr>";
+					codeStl += "<td>Request ID: " + RoomArr[i][0] + "</td>";
+					codeStl += "<td>Year: " + RoomArr[i][1] + "</td>";
+					codeStl += "</tr>";
+					codeStl += "<tr>";
+					codeStl += "<td>Module Code: " + RoomArr[i][2] + "</td>";
+					codeStl += "</tr>";
+					codeStl += "<tr>";
+					codeStl += "<td colspan='2'>" + RoomArr[i][3] + "</td>";
+					codeStl += "</tr>";
+					codeStl += "<tr>";
+					codeStl += "<td>Period: " + RoomArr[i][4] + "</td>";
+					codeStl += "<td>Duration: ADD IN </td>";
+					codeStl += "</tr>";
+					codeStl += "<tr>";
+					
+					/* if (timeFormat==1){
+						var Starttime = startTimeList1.concat(startTimeList2);
+						var Endtime = endTimeList1.concat(endTimeList2)
+					}
+					else{
+						var Starttime = startTimeList1.concat(startTimeList3);
+						var Endtime = endTimeList1.concat(endTimeList3);
+					}
+					var starttime = Starttime[JSON[0].period-1];
+					var endtime = Endtime[(parseInt(JSON[0].period) + parseInt(JSON[0].duration)-2)];
+					
+					codeStl += "<td colspan='2'>Requested Time: " + starttime + "-" + endtime + "</td>"; */
+					codeStl += "</tr>";
+					codeStl += "<tr>";
+					codeStl += "<td>No. Students: " + RoomArr[i][6] + "</td>";
+					codeStl += "<td>No. Rooms: " + RoomArr[i][7] + "</td>";
+					codeStl += "</tr>";
+					codeStl += "<tr>";
+					codeStl += "<td colspan='2'>Booked rooms: "
+					for (var k = 0; k< RoomArr[i][18].length; k++){
+						codeStl += RoomArr[i][18][k] + " ";
+					}
+					codeStl += "</td>";
+					codeStl += "<tr><td colspan='2'>";
+					if(RoomArr[i][8]==1){
+						codeStl += "Quality Room, ";
+					}
+					if(RoomArr[i][9]==1){
+						codeStl += "Wheelchair Access, ";
+					}
+					if(RoomArr[i][10]==1){
+						codeStl += "Data Projector, ";
+					}
+					if(RoomArr[i][11]==1){
+						codeStl += "Double Projector, ";
+					}
+					if(RoomArr[i][12]==1){
+						codeStl += "Visualiser, ";
+					}
+					if(RoomArr[i][13]==1){
+						codeStl += "Video/DVD/Bluray, ";
+					}
+					if(RoomArr[i][14]==1){
+						codeStl += "Computer, ";
+					}
+					if(RoomArr[i][15]==1){
+						codeStl += "White Board, ";
+					}
+					if(RoomArr[i][16]==1){
+						codeStl += "Chalk Board, ";
+					}
+					if(RoomArr[i][17]==1){
+						codeStl += "Nearest Room, ";
+					}
+					codeStl = codeStl.substring(0,codeStl.length-2);
+					codeStl += ".</tr>";
+					codeStl += ".</table>";		
+				}
+				document.getElementById("roomSelectorBox2").innerHTML = (codeStl);
 			}
 			function getTable(week){
 				reqID= [];
@@ -232,20 +294,23 @@
 								case "Friday":day = 5;break;
 							}
 							var count=0;
-							while(count<JSON[i].duration){
+							do{
 								var bubble = parseInt(JSON[i].period)+count;
 								$('#t'+bubble+''+day).removeClass('tabletdVT');
 								$('#t'+bubble+''+day).addClass('changeVT');
-								//$('#t'+bubble+''+day).value='MEOW';
-								document.getElementById("t"+bubble+''+day).innerHTML=JSON[i].modulecode;						
 								var subarray = ['t'+bubble+''+day , JSON[i].requestid];
+								if (document.getElementById("t"+bubble+''+day).innerHTML==""){
+									document.getElementById("t"+bubble+''+day).innerHTML= JSON[i].modulecode;
+								}else{
+									document.getElementById("t"+bubble+''+day).innerHTML= document.getElementById("t"+bubble+''+day).innerHTML+ "<br>" + JSON[i].modulecode;		
+								}								
 								reqID[reqID.length] = subarray;
+								//alert(reqID);
 								count++;
-							}
+							}while(count<JSON[i].duration)
 						}
 					}
 				});
-				
 			}
 		</script>
         <title>View Timetable</title>
